331d5a3a6d8ce71d3d543b3c3b769ac9d818198e
Nitya Bharti <nityabharti123@gmail.com>
2023-11-27 00:11:57 +0530

    slider.

--- src/components/Homepage/Slider.js
+++ src/components/Homepage/Slider.js
@@ -5,14 +5,14 @@ import React from "react";
 import img1 from "../../assets/img_1.png";
 import img2 from "../../assets/img_2.jpg";
 import img3 from "../../assets/img_3.jpg";
-import img4 from "../../assets/img_4.jpg";
+import img4 from "../../assets/img_4.png";
 import img5 from "../../assets/img_5.png";
-import img7 from "../../assets/img_7.png";
+import img6 from "../../assets/img_6.jpg";
 import { styled } from "@mui/material";
 
-// import img6 from "../../assets/img_6.jpg";
+// import img7 from "../../assets/img_7.png";
 
-const image = [img1, img2, img3, img4, img5, img7];
+const image = [img1, img2, img3, img4, img5, img6];
 
 const Image = styled("img")({
   height: "70%",
